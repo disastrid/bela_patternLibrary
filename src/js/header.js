f987f6b78a45ad7e5302d58c8506bffadd83e591
Astrid <astrid@astridbin.com>
2019-09-11 00:32:17 -0400

    Replaced all tabs with spaces for consistency

--- src/js/header.js
+++ src/js/header.js
@@ -1,12 +1,12 @@
 $(".sidebar > a").on("click", function() {
-	if ($(this).hasClass('selected')) {
-		$(this).removeClass('selected');
-	} else {
-	$('#main > .sidebar').find('> a').removeClass('selected');
-	$('#main > .sidebar').find('> span').removeClass('visible');
-		$(this).toggleClass('selected');
-	}
-	var sectionName = $(this).attr('href').substring(1);
-	$('.' + sectionName + '_section').toggleClass('visible');
+  if ($(this).hasClass('selected')) {
+    $(this).removeClass('selected');
+  } else {
+  $('#main > .sidebar').find('> a').removeClass('selected');
+  $('#main > .sidebar').find('> span').removeClass('visible');
+    $(this).toggleClass('selected');
+  }
+  var sectionName = $(this).attr('href').substring(1);
+  $('.' + sectionName + '_section').toggleClass('visible');
 
 });
